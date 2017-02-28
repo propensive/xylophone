a0e9ebdf47217df46bb89454dd031cc4c78ba7ff
Igor Mielientiev <igor.melent@gmail.com>
2017-02-28 11:40:36 +0200

    Fix comments

--- core/src/main/scala/xylophone/macros.scala
+++ core/src/main/scala/xylophone/macros.scala
@@ -10,31 +10,30 @@ private[xylophone] class XmlMacros(val c: whitebox.Context) {
   import c.universe._
 
   def serializerMacro[T: c.WeakTypeTag]: c.Expr[SeqSerializer[T]] = {
+    def generateSerializerForCaseClass(serializer: c.universe.Type): c.Expr[SeqSerializer[T]] = {
+      val decls = getClassFields(weakTypeOf[T]).map(field => findAndCollectSerializerForField(serializer, field))
+      c.Expr[SeqSerializer[T]](
+        q"""
+         new _root_.xylophone.SeqSerializer[${weakTypeOf[T]}] {
+             def serialize(obj: ${weakTypeOf[T]}): XmlSeq =
+                   _root_.xylophone.SeqSerializer.fromMap(_root_.scala.collection.immutable.ListMap(${decls.toSeq: _*}))
+         }
+       """)
+    }
+
     val tpe = weakTypeOf[T].typeSymbol.asClass
     val serializer = typeOf[SeqSerializer[_]].typeSymbol.asType.toTypeConstructor
     if (tpe.isCaseClass) generateSerializerForCaseClass(serializer)
     else throw new Exception()
   }
 
-  private def generateSerializerForCaseClass[T: c.WeakTypeTag](serializer: c.universe.Type): c.Expr[SeqSerializer[T]] = {
-    val decls = getClassFields.map(field => findAndCollectSerializerForField(serializer, field))
-    c.Expr[SeqSerializer[T]](
-      q"""
-         import scala.collection.immutable.ListMap
-         new _root_.xylophone.SeqSerializer[${weakTypeOf[T]}] {
-             def serialize(obj: ${weakTypeOf[T]}): XmlSeq =
-                   _root_.xylophone.SeqSerializer.fromMap(ListMap(${decls.toSeq: _*}))
-         }
-       """)
-  }
-
   private def findAndCollectSerializerForField[T: c.WeakTypeTag](serializer: c.universe.Type, param: c.universe.MethodSymbol) = {
     val implSerializer = c.inferImplicitValue(appliedType(serializer, param.returnType), silent = false, withMacrosDisabled = false)
-    q"""(${param.name.toString} -> $implSerializer.serialize(obj.${param.name}))"""
+    q"""(${param.name.toString}, $implSerializer.serialize(obj.${param.name}))"""
   }
 
-  private def getClassFields[T: c.WeakTypeTag]: Iterable[c.universe.MethodSymbol] = {
-    weakTypeOf[T].decls.collect { case m: MethodSymbol if m.isCaseAccessor => m.asMethod }
+  private def getClassFields(t: c.universe.Type): Iterable[c.universe.MethodSymbol] = {
+    t.decls.collect { case m: MethodSymbol if m.isCaseAccessor => m.asMethod }
   }
 
 }
